043202e19fe5ced86b9095f7babee1f8410df663
mhellar <mark.hellar@gmail.com>
2017-07-27 22:09:33 -0700

    test

--- code_ie/osc_simple/client.js
+++ code_ie/osc_simple/client.js
@@ -1,7 +1,7 @@
 var osc = require('node-osc');
 
-var oscServer = new osc.Server(6448, '0.0.0.0');
-oscServer.on("message", function (msg, rinfo) {
-      console.log("TUIO message:");
-      console.log(msg);
+var oscServer = new osc.Server(8888, '0.0.0.0');
+oscServer.on("message", function(msg, rinfo) {
+    console.log("OSC message:");
+    console.log(msg);
 });
